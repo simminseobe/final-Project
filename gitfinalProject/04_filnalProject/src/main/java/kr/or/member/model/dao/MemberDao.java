2eb943ab21e39d52b054e7d9c442435eb1293ad0
nsh9398 <seunghyeon9398@naver.com>
2023-03-31 15:05:01 +0900

    cache remove

---
+++ gitfinalProject/04_filnalProject/src/main/java/kr/or/member/model/dao/MemberDao.java
@@ -0,0 +1,22 @@
+package kr.or.member.model.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.or.member.model.vo.Member;
+
+@Repository
+public class MemberDao {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	public ArrayList<Member> selectAllMember() {
+		List list = sqlSession.selectList("member.selectAllMember");
+
+		return (ArrayList<Member>) list;
+	}
+}
