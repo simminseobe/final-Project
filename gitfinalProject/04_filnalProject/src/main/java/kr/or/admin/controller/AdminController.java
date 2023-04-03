f2195e0ed61040e8f78ef5517327f66e2d64b216
simminseobe <alstjq047687@gmail.com>
2023-04-03 11:49:43 +0900

    admin.jsp create
    create

--- gitfinalProject/04_filnalProject/src/main/java/kr/or/admin/controller/AdminController.java
+++ gitfinalProject/04_filnalProject/src/main/java/kr/or/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package kr.or.admin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.admin.model.service.AdminService;
 
@@ -9,4 +10,9 @@ import kr.or.admin.model.service.AdminService;
 public class AdminController {
 	@Autowired
 	private AdminService service;
+
+	@RequestMapping("/admin.do")
+	public String admin() {
+		return "admin/admin";
+	}
 }
