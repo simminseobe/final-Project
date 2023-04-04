af6767f7b3c31e75a9e075465317ee32a272956f
simminseobe <alstjq047687@gmail.com>
2023-04-04 12:21:11 +0900

    Update MovieVo

--- gitfinalProject/04_filnalProject/src/main/java/kr/or/movie/model/vo/Movie.java
+++ gitfinalProject/04_filnalProject/src/main/java/kr/or/movie/model/vo/Movie.java
@@ -10,16 +10,15 @@ import lombok.NoArgsConstructor;
 public class Movie {
 	private int moviceNo;
 	private String movieTitle;
+	private String movieDirector;
 	private String movieActor;
 	private String movieGenre;
 	private String movieTime;
 	private String movieDate;
 	private int movieRating;
 	private String movieContent;
-	private int movieAudience;
-	private String movieDistributor;
 	private String fileSociety;
 	private String classicSociety;
-	
-	
+	private String specialMovie;
+	private String enrollMember;
 }
