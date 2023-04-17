56675ed63e6a855e70fcd9f6af9a8134f8dee9d5
simminseobe <alstjq047687@gmail.com>
2023-04-17 16:34:10 +0900

    schedule XML mapper 생성
    생성

---
+++ gitfinalProject/04_filnalProject/src/main/java/kr/or/admin/model/vo/Schedule.java
@@ -0,0 +1,15 @@
+package kr.or.admin.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Schedule {
+	private String movieTitle;
+	private String theaterBranch;
+	private String scheduleStart;
+	private String scheduleEnd;
+}
