60b4f74e916fe8a8fdaf5809474dd158b78c7e19
SHIN-HYEJIN <hyejin950625@gmail.com>
2023-04-29 18:06:36 +0900

    commit

--- gitfinalProject/04_filnalProject/src/main/java/kr/or/member/model/vo/MyMovieStory.java
+++ gitfinalProject/04_filnalProject/src/main/java/kr/or/member/model/vo/MyMovieStory.java
@@ -1,5 +1,15 @@
 package kr.or.member.model.vo;
 
-public class MyMovieStory {
+import java.util.ArrayList;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyMovieStory {
+	private int memberNo;
+	private String memberId;
 }
