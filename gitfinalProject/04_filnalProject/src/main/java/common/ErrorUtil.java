00c0fe6ef610faaa687b2717ec0709e354a187c0
minkyoungjun1 <rudwns4188@gmail.com>
2023-05-03 13:32:13 +0900

    파이널프로젝트 마지막 커밋

--- gitfinalProject/04_filnalProject/src/main/java/common/ErrorUtil.java
+++ gitfinalProject/04_filnalProject/src/main/java/common/ErrorUtil.java
@@ -1,6 +1,10 @@
 package common;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 // @Component를 포함하고 있는 Annotation
 // @Controller Annotation 포함되어 있는 객체에서 에러가 발생했을 때 처리하는 Annotation
@@ -11,22 +15,22 @@ public class ErrorUtil {
 
 	// 500 에러페이지 처리(이때, 실제 소스코드를 수정해야함)
 	// @ExceptionHandler == try - catch
-//	@ExceptionHandler(Exception.class)
-//	public String handleException(Exception e) {
-//		System.err.println("에러발생");
-	// 에러메세지 띄우기
-//		System.err.println(e.getMessage());
-//		return "error/error500";
-//	}
+	@ExceptionHandler(Exception.class)
+	public String handleException(Exception e) {
+	System.err.println("에러발생");
+	 //에러메세지 띄우기
+		System.err.println(e.getMessage());
+		return "error/error500";
+	}
 
 	// 404 에러페이지 처리(실제로 주소를 잘못 입력했을 때)
 	// URL창 없는 주소 입력
-//	@ExceptionHandler(NoHandlerFoundException.class)
-	// HttpStatus.NOT_FOUND : 404
-//	@ResponseStatus(value=HttpStatus.NOT_FOUND)
-//	public String notFound(NoHandlerFoundException e) {
-//		System.err.println("RequestMapping 오류");
-//		System.err.println(e.getMessage());
-//		return "error/error404";
-//	}
+	@ExceptionHandler(NoHandlerFoundException.class)
+	//HttpStatus.NOT_FOUND : 404
+	@ResponseStatus(value=HttpStatus.NOT_FOUND)
+	public String notFound(NoHandlerFoundException e) {
+		System.err.println("RequestMapping 오류");
+		System.err.println(e.getMessage());
+		return "error/error404";
+	}
 }
